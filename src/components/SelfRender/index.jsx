43cbde58c879d55a50c56122c5067559d66cf64d
artemklishch <artemklishch@gmail.com>
2022-04-06 10:08:00 +0300

    made

--- src/components/SelfRender/index.jsx
+++ src/components/SelfRender/index.jsx
@@ -1,23 +1,32 @@
-import { useState, useRef } from "react";
+import { Component } from "react";
 import "./SelfRender.css";
 
-const SelfRender = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [obj, makeForceRender] = useState();
-  const onMakeRerender = () => makeForceRender({});
-  const num = useRef(-1);
-  const onShowRendersNumber = () => {
-    num.current++;
-    alert(`There have been ${num.current} rerenders`);
+class SelfRender extends Component {
+  state = {
+    num: 0,
+    rerender: {},
   };
-  return (
-    <div className="selfrender">
-      <button onClick={onShowRendersNumber}>
-        Show how many rererenders were
-      </button>
-      <button onClick={onMakeRerender}>Make rerender</button>
-    </div>
-  );
-};
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.num === this.state.num) {
+      this.setState((state) => ({
+        num: state.num + 1,
+      }));
+    }
+  }
+  onShowRendersNumber = () => {
+    alert(`Render number - ${this.state.num}`);
+  };
+  onMakeRerender = () => this.setState({ rerender: {} });
+  render() {
+    return (
+      <div className="selfrender">
+        <button onClick={this.onShowRendersNumber}>
+          Show how many rererenders were
+        </button>
+        <button onClick={this.onMakeRerender}>Make rerender</button>
+      </div>
+    );
+  }
+}
 
 export default SelfRender;
